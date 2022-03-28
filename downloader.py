469533a544608c3b48dbb303c678c3b29a06b4dc
Pumpkinjam <rnjsl1212@naver.com>
2022-03-29 01:21:52 +0900

    chore: added some try-except

--- downloader.py
+++ downloader.py
@@ -7,18 +7,26 @@ class Downloader:
     
     def download(self, link: str, file_type: str = 'mp4', file_name: str = "DEFAULT") -> bool:
 
-        stream = YouTube(link).streams;
+        try:
+            stream = YouTube(link).streams;
+        except:
+            print("Cannot find such link:", link);
+            return False;
+
         if (file_name == "DEFAULT"):
             file_name = stream[0].title;
         
-        if (file_type == 'mp4'):
-            video = stream.filter(progressive=True, file_extension=file_type);
-            video.get_highest_resolution().download(output_path= self.path, filename= (file_name + '.' + file_type));
-        elif (file_type == 'mp3'):
-            audio = stream.filter(file_extension=file_type, only_audio=True, subtype='webm', abr='160kbps');
-            audio.first().download(output_path= self.path, filename= (file_name + '.' + file_type));
+        try:
+            if (file_type == 'mp4'):
+                video = stream.filter(progressive=True, file_extension=file_type);
+                video.get_highest_resolution().download(output_path= self.path, filename= (file_name + '.' + file_type));
+            elif (file_type == 'mp3'):
+                audio = stream.filter(file_extension=file_type, only_audio=True, subtype='webm', abr='160kbps');
+                audio.first().download(output_path= self.path, filename= (file_name + '.' + file_type));
+        except:
+            print("Oops, some error occured while downloading.");
+            return False;
         
-
         print(f"{file_name}.{file_type}: Download Complete.");
         return True;
         
--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import downloader as ydl
 
-download_dir = input("Input Download Directory (use '.' for default): ");
-
 tmp = int(input(
 """Select the file type:
 1. mp4
 2. mp3
 >>> """
 ));
-
 ftype = 'mp4' if tmp == 1 else 'mp3';
 
-dl = ydl.Downloader(download_dir);
+download_dir = input("Input Download Directory (use '/' for default): ");
+
+dl = ydl.Downloader('../dltube/download' if download_dir == '/' else download_dir);
 
 while(True):
     link = input("Input youtube link (input 'quit' for exiting program): ");
     if link == 'quit':
         break;
     
-    name = input("Input file name (use '*' for using default name): ");
-    if name == '*':
+    name = input("Input file name (use '/' for using default name): ");
+    if name == '/':
         dl.download(link, ftype);
     else:
         dl.download(link, ftype, name);
